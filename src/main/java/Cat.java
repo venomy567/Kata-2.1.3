2f3655813bc95f50f4d62bfe5fe0337fe2b0182f
venomy567 <47823187+venomy567@users.noreply.github.com>
2023-03-19 12:21:29 +0300

    asd

---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+public class Cat {
+    private String name;
+    public Cat(String name) {
+        this.name = name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+
+
+
+}
